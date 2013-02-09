958404bf0251bb4dc2c5a00d21570c36a5fafc8b
c-w <clemens.wolff+github@gmail.com>
2013-02-09 18:20:11 +0000

    Output normalised image in uint8 format.
    Fix for consistency since input images are in uint8 format.

--- src/rgb_normalise.m
+++ src/rgb_normalise.m
@@ -5,8 +5,8 @@ green = double(image(:,:,2));
 blue = double(image(:,:,3));
 
 euclid_rgb = sqrt(red(:,:).^2 + green(:,:).^2 + blue(:,:).^2);
-normalised_red = red(:,:)./euclid_rgb;
-normalised_green = green(:,:)./euclid_rgb;
-normalised_blue = blue(:,:)./euclid_rgb;
+normalised_red = uint8(round(red(:,:)./euclid_rgb.*255));
+normalised_green = uint8(round(green(:,:)./euclid_rgb.*255));
+normalised_blue = uint8(round(blue(:,:)./euclid_rgb.*255));
 
 normalised_image = cat(3, normalised_red, normalised_green, normalised_blue);
