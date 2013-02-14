9ab23ba5143f9045affdb950b4b1d99adb86d257
c-w <clemens.wolff+github@gmail.com>
2013-02-14 15:56:07 +0000

    Parameter-less version of random_image()
    Returns an image in ug3_Vision/data/RND_FOLDER/RND_FILE.jpg where RND_FOLDER is
    a randomly selected subfolder of ug3_Vision/data and RND_FILE is a random file
    in that folder.

--- src/random_image.m
+++ src/random_image.m
@@ -1,12 +1,49 @@
+% returns a random image of type |image_type| found at |path|
+% if |path| is not specified, set it to a random folder in "ug3_Vision/data"
 function image = random_image(path, image_type)
+    if nargin < 1
+        path = random_dir('ug3_Vision', 'data');
+    end
     if nargin < 2
-        image_type = 'jpg';
+        image_type = '.jpg';
     end
 
-    files = dir(sprintf('%s/*.%s', path, image_type));
-    filenames = {files.name};
-    [~, num_files] = size(filenames);
+    if ~strcmp(image_type(1), '.')
+        image_type = strcat('.', image_type);
+    end
 
-    random_index = randi([1 num_files]);
-    filename = sprintf('%s/%s', path, filenames{random_index});
-    image = imread(filename);
+    image = imread(random_file(path, image_type));
+end
+
+
+% looks for a directory |tl_dir| somewhere up from this file's location
+% returns the absolute path to one of the directories in |tl_dir|/|branch_dir|
+function path = random_dir(tl_dir, branch_dir)
+    cur_path = mfilename('fullpath');
+    tl_path = cur_path(1:strfind(cur_path, tl_dir) + length(tl_dir));
+    branch_path = strcat(tl_path, branch_dir);
+    branch_path_contents = dir(branch_path);
+    branch_path_dirs = {};
+    for c = 1 : length(branch_path_contents)
+        elem = branch_path_contents(c);
+        if ~elem.isdir || strcmp(elem.name, '.') || strcmp(elem.name, '..')
+            continue;
+        end
+        branch_path_dirs{end + 1} = fullfile(branch_path, elem.name);
+    end
+    path = branch_path_dirs{randi([1 length(branch_path_dirs)])};
+end
+
+
+% returns a random file ending with |extension| found at |path|
+function path = random_file(path, extension)
+    if nargin < 2
+        extension = '';
+    end
+
+    file_type = strcat('*', extension);
+    files = dir(fullfile(path, file_type));
+    filenames = {files.name};
+
+    path = fullfile(path, filenames{randi([1 length(filenames)])});
+end
