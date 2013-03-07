ad8b7fee426a2cd67917a04f2201fc4110089dbb
c-w <clemens.wolff+github@gmail.com>
2013-03-07 05:51:29 +0000

    Fully automated report make script

--- report/make.sh
+++ report/make.sh
@@ -1,23 +1,41 @@
 #!/bin/bash
 
-base_dir="$(dirname $(readlink -f $0))"
-src_dir="$base_dir/../src"
+report_dir="$(dirname $(readlink -f $0))"
+code_dir="$(readlink -f $report_dir/../src)"
+src_dir="$report_dir/src"
 
-if [ -z "$1" ]; then
-    arg="report.tex"
-else
-    arg="$1"
+tex_file="$src_dir/report.tex"
+matlab_file="$src_dir/code.m"
+pdf_file="$src_dir/$(basename $tex_file .tex).pdf"
+report_file="$report_dir/$(basename $tex_file .tex)-full.pdf"
+
+# print source files to HTML and then to PDF
+if [ -f $matlab_file ]; then
+    rm $matlab_file
 fi
-
-# print source files to HTML
-mfile="$src_dir/.all.m"
+for f in $(find $code_dir -maxdepth 1 -type f -name '*.m' | sort); do
+    cat $f >> $matlab_file
+    echo >> $matlab_file
+    echo >> $matlab_file
+done
+for f in $(find $code_dir -mindepth 2 -type f -name '*.m' -not -path '*/.*/*' \
+           | sort); do
+    cat $f >> $matlab_file
+    echo >> $matlab_file
+    echo >> $matlab_file
+done
+head -n -2 $matlab_file > $matlab_file.tmp
+mv $matlab_file.tmp $matlab_file
 vim -e +':colorscheme shine' +:TOhtml +':%s/#00ffff/#000000/g' \
-    +':%s/#ffff00/#0000ff/g' +w +qa $mfile
-mv "$mfile.html" "$base_dir/code.m.html"
+    +':%s/#ffff00/#0000ff/g' +w +qa $matlab_file
+html2ps --colour $matlab_file.html > $matlab_file.ps
+ps2pdf -sPAPERSIZE=a4 -dEmbedAllFonts=true -dOptimize=true \
+    -dAutoRotatePages=/None $matlab_file.ps $matlab_file.pdf
 
-# compile and count words
-pdflatex $arg &&\
-clear &&\
-echo "$arg: $(ps2ascii report.pdf | wc -w) words"
+# compile report
+pdflatex -output-directory $src_dir $tex_file
+pdflatex -output-directory $src_dir $tex_file
+pdfjoin --rotateoversize 'false' $pdf_file $matlab_file.pdf -o $report_file
 
-pdfjoin report.pdf code.pdf -o report-full.pdf
+# count words
+echo "$report_file: $(ps2ascii $pdf_file | wc -w) words"
--- src/main.m
+++ src/main.m
@@ -9,55 +9,61 @@ function res = main(path, image_type, start_offset, time_step)
         time_step = 0.33;
     end
 
+    _path = mfilename('fullpath');
+    [_path, _, _] = fileparts(_path);
+    addpath(fullfile(_path, 'algo'));
+    addpath(fullfile(_path, 'draw'));
+    addpath(fullfile(_path, 'test'));
+
     files = dir(sprintf('%s/*.%s', path, image_type));
     filenames = {files.name};
     [~, num_files] = size(filenames);
-    
+
     [m n ~] = size(imread(sprintf('%s/%s', path, filenames{1})));
     track_mask = zeros(m, n);
-    
+
     rc = zeros(num_files - start_offset, 2);
     gc = zeros(num_files - start_offset, 2);
     bc = zeros(num_files - start_offset, 2);
-    
+
     reds = cell(num_files - start_offset, 1);
     greens = cell(num_files - start_offset, 1);
     blues = cell(num_files - start_offset, 1);
-       
+
     for i = 1 + start_offset : num_files
         image = imread(sprintf('%s/%s', path, filenames{i}));
         [directions, centroids] = analyse_image(image);
-         
+
         r = uint16(centroids(1,:));
         if r(1) > 0 && r(2) > 0
             track_mask = overlay_cross(track_mask, 1, r(1), r(2));
             rc(i,:) = [r(1), r(2)];
         end
-        
+
         g = uint16(centroids(2,:));
         if g(1) > 0 && g(2) > 0
             track_mask = overlay_cross(track_mask, 2, g(1), g(2));
             gc(i,:) = [g(1), g(2)];
         end
 
-  b = uint16(centroids(3,:));
+        b = uint16(centroids(3,:));
         if b(1) > 0 && b(2) > 0
             track_mask = overlay_cross(track_mask, 3, b(1), b(2));
             bc(i,:) = [b(1), b(2)];
         end
-        
+
         reds{i} = image(:,:,1);
         greens{i} = image(:,:,2);
         blues{i} = image(:,:,3);
         imshow(overlay_mask(image, directions));
-        
+
         pause(time_step);
     end
-    
+
     rc = rc(any(rc,2),:);
     gc = gc(any(gc,2),:);
     bc = bc(any(bc,2),:);
-    
+
     red_median = median(cat(3, reds{:}), 3);
     green_median = median(cat(3, greens{:}), 3);
     blue_median = median(cat(3, blues{:}), 3);
